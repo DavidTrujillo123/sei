fa2158b027f4c87fc7d34b17a2b44deb049c390a
David Trujillo <davot098@gmail.com>
2023-12-12 19:07:56 -0500

    database conexion update

--- API/config/connection.js
+++ API/config/connection.js
@@ -1,12 +1,25 @@
 const pgPromise = require ('pg-promise');
-const config = {
-    host: 'localhost',
+// const config = {
+//     host: 'localhost',
+//     port: '5432',
+//     database: 'sei_2023',
+//     user: 'admin_sei_2023',
+//     password: 'jZgKS6eWe8HqEP3FwptPa3gUeAf3QZzz'
+// };
+
+const config= {
+    host: 'dpg-clsck2d5jf6s73ahdui0-a.oregon-postgres.render.com',
     port: '5432',
-    database: 'pizaas',
-    user: 'postgres',
-    password: '200113'
-};
+
+    database: 'sei_2023',
+    user: 'admin_sei_2023',
+    password: 'jZgKS6eWe8HqEP3FwptPa3gUeAf3QZzz',
+    ssl: true
+}
+
 const pgp = pgPromise({});
 const db = pgp(config);
+
+db.any("select * from audit").then((res)=>{console.log(res);})
 
 exports.db = db;
