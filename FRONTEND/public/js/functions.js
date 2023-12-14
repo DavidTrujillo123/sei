e7dc857766912c1e2be27ad98a2b943b547143cd
David Trujillo <davot098@gmail.com>
2023-12-13 22:41:46 -0500

    Change framework to svelte

--- FRONTEND/public/js/functions.js
+++
@@ -1,8 +0,0 @@
-function validateRechapta(obj){
-    const captchaResponse = grecaptcha.getResponse();
-    if(captchaResponse.length === 0){
-        alert("Por Favor Realize el reCaptcha");
-    }else{
-        document.getElementById(obj).submit();
-    }
-};
