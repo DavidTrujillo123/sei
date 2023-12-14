c0823f4c37feacd995e2a4e736695f0c0ff33797
David Trujillo <davot098@gmail.com>
2023-12-13 19:35:12 -0500

    Add function admin.controller

--- BACKEND/controllers/admin.controller.js
+++ BACKEND/controllers/admin.controller.js
@@ -1,36 +1,40 @@
-const {} = require("express");
+const { } = require("express");
 const { db } = require("../config/connection");
 
 const postUser = async (req, res) => {
-    const {rol_id_p,us_name_p ,
-        us_surname_p,us_email_p,
-        us_password_p,
-        us_state_p,us_id_created} = req.query;
-    try {
-      const response = await db.one(
-        `CALL insert_users($1, $2, $3, $4, $5, %6, $7);`,
-        [rol_id_p,us_name_p ,
-         us_surname_p,us_email_p,
-         us_password_p,
-         us_state_p,us_id_created]
-      );
-      res.json({
-        message: "Usuario creado con exito!!",
-        // body: {
-        //   blog: {
-        //     response,
-        //   },
-        // },
-      });
-    } catch (error) {
-      res.json({
-        message: "Error  Fatal al crear el Comentario!!",
-        error: { error },
-      });
-    }
-  };
+  const { rol_id_p, us_name_p,
+    us_surname_p, us_email_p,
+    us_password_p, us_id_created } = req.query;
+  try {
+    const response = await db.any(
+      `CALL insert_users($1, $2, $3, $4, $5, true, $6);`,
+      [rol_id_p, us_name_p, us_surname_p,
+        us_email_p, us_password_p, us_id_created]
+    );
+    res.json({
+      message: "Usuario creado con exito!!",
+      body: {
+        user: {
+          response: {
+            rol_id: rol_id_p,
+            us_name: us_name_p,
+            us_surname: us_surname_p,
+            us_email: us_email_p,
+            us_password: us_password_p,
+            us_state: true,
+          },
+        },
+      },
+    });
+  } catch (error) {
+    res.json({
+      message: "Error al crear el Usuario!!",
+      error: { error },
+    });
+  }
+};
 
 
 module.exports = {
-    postUser
+  postUser
 }
--- BACKEND/routes/index.js
+++ BACKEND/routes/index.js
@@ -2,7 +2,7 @@ const {Router} = require('express');//funcion de expres para administrar rutas
 const router = Router();
 const {postUser} = require('../controllers/admin.controller');
 
-router.post('/admin', postUser);
+router.post('/user', postUser);
 
 
 module.exports = router;
