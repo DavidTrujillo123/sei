8daf3c34d8f7c58b48abe7ced4d3ed4ad777933d
CristianKenedy <cristian181996@gmail.com>
2023-12-13 19:44:33 -0500

    Chapta

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -1,3 +1,23 @@
+const express = require('express');
+const app = express();
 // Motor de plantilla
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
+
+//middlewears
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//Routes
+app.get('/', (req,res) => {
+    res.render('login', {title: 'Login SEI'})
+});
+
+
+//Service Ejecution
+
+app.listen(3000);
+console.log('Server running in: http://localhost:3000');
+
+
+
