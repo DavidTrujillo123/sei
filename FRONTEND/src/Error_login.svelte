c7f28cf1b5ec5eff02d31ad700c8a2928b22f3a7
Cristian Castro <cristian181996@gmail.com>
2023-12-15 09:39:57 -0500

    Update Error

--- FRONTEND/src/Error_login.svelte
+++ FRONTEND/src/Error_login.svelte
@@ -1,1 +1,34 @@
-<h1>Usuario incorrecto</h1>
+<script>
+  import Form from "./Form.svelte";
+</script>
+
+<main>
+ <div class="contain_error_login">
+     <div class="message_error">Error en el Usuario o en la Contrasenia</div>
+   </div>
+   
+   <h1>error</h1>
+   
+   <Form />
+
+</main>
+
+
+
+
+<style>
+.message_error {
+ font-size: 20px;
+ font-weight: 700;
+ color: #e8eaee;
+ text-align: center;
+ font-family: 'Karla', sans-serif;
+}
+.contain_error_login{
+ display: block;
+ background-color: rgb(156, 53, 53);
+ padding: 20px;
+ margin-left: 20%;
+ margin-right: 20%;
+}
+</style>
