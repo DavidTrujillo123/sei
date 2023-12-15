bdb2368ce928a42380e80a4f9c86f4c29d02411b
Jordi7-7 <jordi.cahuas20@gmail.com>
2023-12-15 14:47:31 -0500

    BACKEND select administradores

--- BACKEND/controllers/admin.controller.js
+++ BACKEND/controllers/admin.controller.js
@@ -59,9 +59,87 @@ const select_users = async (req, res) => {
   }
 };
 
+const select_products = async (req, res) => {
+  const adm_id  = req.params.adm_id;
+  console.log(adm_id);
+  try {
+    const response = await db.any(
+      `SELECT * FROM select_product($1);`,
+      [adm_id]
+    );
+    if (response) {
+      res.json({
+        response
+      });
+    } else {
+      res.json({
+        res: "Productos no encontrados"
+      });
+    }
+  } catch (error) {
+    res.json({
+      res: "ERROR",
+      error: error.message
+    });
+  }
+};
+
+const select_roles = async (req, res) => {
+  const adm_id  = req.params.adm_id;
+  console.log(adm_id);
+  try {
+    const response = await db.any(
+      `SELECT * FROM select_role($1);`,
+      [adm_id]
+    );
+    if (response) {
+      res.json({
+        response
+      });
+    } else {
+      res.json({
+        res: "Roles no encontrados"
+      });
+    }
+  } catch (error) {
+    res.json({
+      res: "ERROR",
+      error: error.message
+    });
+  }
+};
+
+const select_shopping_order = async (req, res) => {
+  const adm_id  = req.params.adm_id;
+  console.log(adm_id);
+  try {
+    const response = await db.any(
+      `SELECT * FROM select_shopping_order($1);`,
+      [adm_id]
+    );
+    if (response) {
+      res.json({
+        response
+      });
+    } else {
+      res.json({
+        res: "Ordenes no encontradas"
+      });
+    }
+  } catch (error) {
+    res.json({
+      res: "ERROR",
+      error: error.message
+    });
+  }
+};
+
 
 
 module.exports = {
   postUser,
-  select_users
+  select_users,
+  select_products,
+  select_roles,
+  select_shopping_order
 }
--- BACKEND/routes/index.js
+++ BACKEND/routes/index.js
@@ -1,16 +1,26 @@
 const {Router} = require('express');//funcion de expres para administrar rutas
 const router = Router();
-const {postUser,select_users} = require('../controllers/admin.controller');
+const {postUser,
+    select_users,
+select_products,
+select_roles,
+select_shopping_order} = require('../controllers/admin.controller');
 const {getProducts} = require('../controllers/general.controller');
 
 const {
     isUser
 } = require('../controllers/users.controller');
 
-router.post('/isUser',isUser);
+
 
 router.post('/getProducts',getProducts);
 
+
+//RUTAS DE ADMINISTRADOR
+router.post('/isUser',isUser);
 router.get('/selectUsers/:adm_id',select_users);
+router.get('/selectProducts/:adm_id',select_products);
+router.get('/selectRoles/:adm_id',select_roles);
+router.get('/selectOrders/:adm_id',select_shopping_order);
 
 module.exports = router;
