40fa42fc66fb4a1393540c15af0e2c6844132518
David Trujillo <davot098@gmail.com>
2023-12-15 17:56:18 -0500

    Fix bugs

--- BACKEND/controllers/general.controller.js
+++ BACKEND/controllers/general.controller.js
@@ -8,17 +8,89 @@ const getProducts = async (req, res) => {
             [us_id]
         );
         res.json({
-            state: 'TRUE',
+            state: true,
             product: response
         });
     } catch (error) {
       res.json({
-        state: 'FALSE',
+        state: false,
         error: error.message
       });
     }
 }
 
+const select_products = async (req, res) => {
+    const adm_id  = req.params.adm_id;
+    try {
+      const response = await db.any(
+        `SELECT * FROM select_product($1);`,
+        [adm_id]
+      );
+      if (response) {
+        res.json({
+          response
+        });
+      } else {
+        res.json({
+          res: "Productos no encontrados"
+        });
+      }
+    } catch (error) {
+      res.json({
+        res: "ERROR",
+        error: error.message
+      });
+    }
+  };
+
+  const select_shopping_order = async (req, res) => {
+    const adm_id  = req.params.adm_id;
+    try {
+      const response = await db.any(
+        `SELECT * FROM select_shopping_order($1);`,
+        [adm_id]
+      );
+      if (response) {
+        res.json({
+          response
+        });
+      } else {
+        res.json({
+          res: "Ordenes no encontradas"
+        });
+      }
+    } catch (error) {
+      res.json({
+        res: "ERROR",
+        error: error.message
+      });
+    }
+  };
+
+  const select_det_shopping_order = async (req, res) => {
+    const adm_id  = req.params.adm_id;
+    const ord_id =  req.query.ord_id;
+    try {
+      const response = await db.any(
+        `SELECT * FROM select_det_shopping_order($1,$2);`,
+        [adm_id,ord_id]
+      );
+      if (response) {
+        res.json({
+          response
+        });
+      } else {
+        res.json({
+          res: "Ordenes no encontradas"
+        });
+      }
+    } catch (error) {
+      res.json({
+        res: "ERROR",
+        error: error.message
+      });
+    }
+  };
 module.exports = {
     getProducts
 }
