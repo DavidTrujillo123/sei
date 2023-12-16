f23e0115895f677a7bc84883d15f595a4d94088e
David Trujillo <davot098@gmail.com>
2023-12-16 17:53:13 -0500

    Finish CRUD function in Product model

--- FRONTEND/Model/Product.mjs
+++ FRONTEND/Model/Product.mjs
@@ -8,10 +8,21 @@ export default class Product {
         return response;
     }
     async postProduct(obj_data){
-        const urlApi = 'http://localhost:3000/selectProducts';
+        const urlApi = 'http://localhost:3000/insertProduct';
         const conn = new Conexion;
-        const response = await conn.getData(urlApi, obj_data);
+        const response = await conn.postData(urlApi, obj_data);
         return response;
     }
-
+    async updateProduct(obj_data){
+        const urlApi = 'http://localhost:3000/updateProduct';
+        const conn = new Conexion;
+        const response = await conn.putData(urlApi, obj_data);
+        return response;
+    }
+    async deleteProduct(obj_data){
+        const urlApi = 'http://localhost:3000/deleteProduct';
+        const conn = new Conexion;
+        const response = await conn.deleteData(urlApi, obj_data);
+        return response;
+    }
 }
